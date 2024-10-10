b09881125013ae8ada68d82aaf5e43c66025aa96
Vihanga <vihanga@ftservices.net>
2024-10-10 09:44:24 +0530

    Project Initiation

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SAK_IndexPage
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAK_IndexPage.MvcApplication" Language="C#" %>
